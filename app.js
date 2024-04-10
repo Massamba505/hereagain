2122c7dbf2acb26d90e4104081740d6358ae8cc7
Massamba505 <sabelomaphalala131@gmail.com>
2024-04-10 23:22:33 +0200

    Revert "adding what is needed for the lab"
    This reverts commit d63562d69abc5b746528ed688e5f02c32a8a1a6c.

--- app.js
+++ app.js
@@ -14,46 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const cars = require('./cars.json');
-
-//get all cars
-app.get('/cars', (req, res) => {
-    res.json(cars);
-});
-
-//get car by id
-app.get('/cars/:id', (req, res) => {
-    const id = req.params.id;
-    const car = cars.find(car => car.id === id);
-    res.json(car);
-});
-
-//update car
-app.put('/cars/:id', (req, res) => {
-    const id = req.params.id;
-    const updatedCar = req.body;
-    const index = cars.findIndex(car => car.id === id);
-    cars[index] = updatedCar;
-    res.json(updatedCar);
-});
-
-//delete car
-app.delete('/cars/:id', (req, res) => {
-    const id = req.params.id;
-    const index = cars.findIndex(car => car.id === id);
-    cars.splice(index, 1);
-    res.json({ message: `Car with id ${id} deleted` });
-});
-
-//add car
-app.post('/cars', (req, res) => {
-    console.log(req);
-    const newCar = req.body;
-    console.log(newCar);
-    cars.push(newCar);
-    res.json(newCar);
-});
-
-
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 module.exports = app;
