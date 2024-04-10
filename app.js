02f949c54b704f962b2c4bb1f3cecb674bf70f3a
Massamba505 <sabelomaphalala131@gmail.com>
2024-04-10 23:32:11 +0200

    app.js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -17,5 +17,49 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-module.exports = app;
+
+const cars = require('./cars.json');
+
+//get all cars
+app.get('/cars', (req, res) => {
+    res.json(cars);
+});
+
+//get car by id
+app.get('/cars/:id', (req, res) => {
+    const id = req.params.id;
+    const car = cars.find(car => car.id === id);
+    res.json(car);
+});
+
+//update car
+app.put('/cars/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedCar = req.body;
+    const index = cars.findIndex(car => car.id === id);
+    cars[index] = updatedCar;
+    res.json(updatedCar);
+});
+
+//delete car
+app.delete('/cars/:id', (req, res) => {
+    const id = req.params.id;
+    const index = cars.findIndex(car => car.id === id);
+    cars.splice(index, 1);
+    res.json({ message: `Car with id ${id} deleted` });
+});
+
+//add car
+app.post('/cars', (req, res) => {
+    console.log(req);
+    const newCar = req.body;
+    console.log(newCar);
+    cars.push(newCar);
+    res.json(newCar);
+});
+
+
+
+module.exports = app;exports = app;
+;
 
